0a33262e6d719bdc4a3dc501cfc7444abd4705f5
pashaGhub <pawelsucharew@gmail.com>
2020-01-17 01:15:52 +0200

    React-Context implemented && live title changin functionality added

--- src/AppContext.tsx
+++ src/AppContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 interface IResult {
   numToDisplay: string;
@@ -9,8 +9,13 @@ interface IResult {
   breakpoint: boolean;
 }
 
+interface IOperator {
+  status: boolean;
+  sign: string;
+}
 interface IState {
   result: IResult;
+  operator: IOperator;
 }
 
 const initialState: IState = {
@@ -21,16 +26,148 @@ const initialState: IState = {
     dotAtEnd: false,
     res: 0,
     breakpoint: false
+  },
+  operator: {
+    status: true,
+    sign: ""
   }
 };
 
-export const AppContext = createContext<IState>(initialState);
-
-function reducer() {}
+export const AppContext = createContext<IState | any>(initialState);
 
 export function ContextProvider(props: any): JSX.Element {
+  const [result, setResult] = useState<IResult>(initialState.result);
+  const [operator, setOperator] = useState<IOperator>(initialState.operator);
+  const [title, setTitle] = useState<string>("Welcome");
+
+  // display result on screen
+  function numberTyping(digit: string): void {
+    if (result.breakpoint) {
+      clean();
+    } else {
+      if (operator.status) {
+        digit === "."
+          ? setResult({
+              ...result,
+              numToDisplay: "0" + digit,
+              dotInNum: true,
+              dotAtEnd: true
+            })
+          : setResult({ ...result, numToDisplay: digit });
+      } else if (!result.dotInNum) {
+        digit === "."
+          ? setResult({
+              ...result,
+              numToDisplay: result.numToDisplay + digit,
+              dotInNum: true,
+              dotAtEnd: true
+            })
+          : setResult({ ...result, numToDisplay: result.numToDisplay + digit });
+      } else if (digit !== ".") {
+        setResult({
+          ...result,
+          numToDisplay: result.numToDisplay + digit,
+          dotAtEnd: false
+        });
+      }
+      setOperator({ ...operator, status: false });
+    }
+  }
+
+  // handle operator click
+  function operatorToUse(newOperator: string): void {
+    calculate();
+
+    //removing dot at the end of the digit
+    const checkedDigit: string = result.dotAtEnd
+      ? result.numToDisplay.slice(0, -1)
+      : result.numToDisplay;
+
+    if (result.breakpoint) {
+      setResult({
+        ...result,
+        smallScreen: result.res.toString(),
+        breakpoint: false
+      });
+    } else if (!result.smallScreen) {
+      setResult({
+        ...result,
+        numToDisplay: checkedDigit,
+        smallScreen: checkedDigit,
+        dotInNum: false,
+        dotAtEnd: false,
+        res: Number(checkedDigit)
+      });
+    } else if (newOperator === "=") {
+      setResult({
+        numToDisplay: calculate().toString(),
+        smallScreen: result.smallScreen + operator.sign + checkedDigit,
+        dotInNum: false,
+        dotAtEnd: false,
+        res: calculate(),
+        breakpoint: true
+      });
+    } else if (!operator.status) {
+      setResult({
+        ...result,
+        numToDisplay: calculate().toString(),
+        smallScreen: result.smallScreen + operator.sign + checkedDigit,
+        dotInNum: false,
+        dotAtEnd: false,
+        res: calculate()
+      });
+    }
+
+    setOperator({ status: true, sign: newOperator });
+  }
+
+  // execute the canculation
+  function calculate(): number {
+    switch (operator.sign) {
+      case "+":
+        return result.res + Number(result.numToDisplay);
+      case "-":
+        return result.res - Number(result.numToDisplay);
+      case ":":
+        return result.res / Number(result.numToDisplay);
+      case "*":
+        return result.res * Number(result.numToDisplay);
+      default:
+        return result.res;
+    }
+  }
+
+  // handle clean button
+  function clean(): void {
+    setResult({
+      numToDisplay: "0",
+      smallScreen: false,
+      dotInNum: false,
+      dotAtEnd: false,
+      res: 0,
+      breakpoint: false
+    });
+    setOperator({ status: true, sign: "" });
+  }
+
+  // handle title changing
+  function handleTitleChange(newTitle: string): void {
+    setTitle(newTitle);
+    clean();
+  }
+
   return (
-    <AppContext.Provider value={initialState}>
+    <AppContext.Provider
+      value={{
+        result,
+        operator,
+        numberTyping,
+        operatorToUse,
+        clean,
+        title,
+        handleTitleChange
+      }}
+    >
       {props.children}
     </AppContext.Provider>
   );
--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import { CalcButton } from "./Button";
 import "../App.scss";
 import { AppContext } from "../AppContext";
@@ -23,152 +23,19 @@ const characters: string[] = [
   ""
 ];
 
-interface IResult {
-  numToDisplay: string;
-  smallScreen: string | boolean;
-  dotInNum: boolean;
-  dotAtEnd: boolean;
-  res: number;
-  breakpoint: boolean;
-}
-
-interface IOperator {
-  status: boolean;
-  sign: string;
-}
-
 export const Calculator: React.FC = () => {
-  const store = useContext(AppContext);
-
-  console.log(store);
-
-  const [result, setResult] = useState<IResult>({
-    numToDisplay: "0",
-    smallScreen: false,
-    dotInNum: false,
-    dotAtEnd: false,
-    res: 0,
-    breakpoint: false
-  });
-
-  const [operator, setOperator] = useState<IOperator>({
-    status: true,
-    sign: ""
-  });
-
-  // display result on screen
-  function numberTyping(digit: string): void {
-    if (result.breakpoint) {
-      clean();
-    } else {
-      if (operator.status) {
-        digit === "."
-          ? setResult({
-              ...result,
-              numToDisplay: "0" + digit,
-              dotInNum: true,
-              dotAtEnd: true
-            })
-          : setResult({ ...result, numToDisplay: digit });
-      } else if (!result.dotInNum) {
-        digit === "."
-          ? setResult({
-              ...result,
-              numToDisplay: result.numToDisplay + digit,
-              dotInNum: true,
-              dotAtEnd: true
-            })
-          : setResult({ ...result, numToDisplay: result.numToDisplay + digit });
-      } else if (digit !== ".") {
-        setResult({
-          ...result,
-          numToDisplay: result.numToDisplay + digit,
-          dotAtEnd: false
-        });
-      }
-      setOperator({ ...operator, status: false });
-    }
-  }
-
-  // handle operator click
-  function operatorToUse(newOperator: string): void {
-    calculate();
-
-    //removing dot at the end of the digit
-    const checkedDigit: string = result.dotAtEnd
-      ? result.numToDisplay.slice(0, -1)
-      : result.numToDisplay;
-
-    if (result.breakpoint) {
-      setResult({
-        ...result,
-        smallScreen: result.res.toString(),
-        breakpoint: false
-      });
-    } else if (!result.smallScreen) {
-      setResult({
-        ...result,
-        numToDisplay: checkedDigit,
-        smallScreen: checkedDigit,
-        dotInNum: false,
-        dotAtEnd: false,
-        res: Number(checkedDigit)
-      });
-    } else if (newOperator === "=") {
-      setResult({
-        numToDisplay: calculate().toString(),
-        smallScreen: result.smallScreen + operator.sign + checkedDigit,
-        dotInNum: false,
-        dotAtEnd: false,
-        res: calculate(),
-        breakpoint: true
-      });
-    } else if (!operator.status) {
-      setResult({
-        ...result,
-        numToDisplay: calculate().toString(),
-        smallScreen: result.smallScreen + operator.sign + checkedDigit,
-        dotInNum: false,
-        dotAtEnd: false,
-        res: calculate()
-      });
-    }
-
-    setOperator({ status: true, sign: newOperator });
-  }
-
-  // execute the canculation
-  function calculate(): number {
-    switch (operator.sign) {
-      case "+":
-        return result.res + Number(result.numToDisplay);
-      case "-":
-        return result.res - Number(result.numToDisplay);
-      case ":":
-        return result.res / Number(result.numToDisplay);
-      case "*":
-        return result.res * Number(result.numToDisplay);
-      default:
-        return result.res;
-    }
-  }
-
-  // handle clean button
-  function clean(): void {
-    setResult({
-      numToDisplay: "0",
-      smallScreen: false,
-      dotInNum: false,
-      dotAtEnd: false,
-      res: 0,
-      breakpoint: false
-    });
-    setOperator({ status: true, sign: "" });
-  }
+  const {
+    result,
+    operator,
+    numberTyping,
+    clean,
+    operatorToUse,
+    title
+  } = useContext(AppContext);
 
   return (
     <div className="calculator-wrapper">
-      <h2 className="calculator-title">Fix me!</h2>
+      <h2 className="calculator-title">{title}</h2>
       <div className="calculator">
         <div>
           <h3 className="result-small">
